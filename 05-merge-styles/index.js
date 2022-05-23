d40f555af690f47234e642ae466c426070555329
y1ungspace <y1ung.space@gmail.com>
2022-05-23 15:44:10 +0200

    feat: solve 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = path.join(__dirname, 'styles');
+const output = fs.createWriteStream(path.join(__dirname, 'project-dist/bundle.css'));
+
+
+ fs.readdir(input, (err, files) => {
+    if (err) throw err;
+    else {
+        files.forEach(file => {
+            if (path.extname(file) === '.txt') return
+            let currentFile = fs.createReadStream(path.join(input, file), 'utf-8')
+            currentFile.on('data', chunk => output.write(chunk));
+        })
+    }   
+})
+
+console.log('Successfully merged all css files ^^')
+
+
