3bb5a206dc4a6ef5f2406ae43888653c2d4d8d02
y1ungspace <y1ung.space@gmail.com>
2022-05-23 13:25:03 +0200

    fear: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = path.join(__dirname, 'files');
+const output = path.join(__dirname, 'files-copy');
+
+fs.mkdir(output , {recursive: true}, err => {
+    if (err) throw err;
+    else {
+        fs.readdir(input, (err, files) => {
+            if (err) throw err;
+            else {
+                files.forEach(file => {
+                    fs.writeFile(
+                        path.join(output, file),
+                        '',
+                        (err) => {
+                            if (err) throw err;
+                        }
+                    );
+
+                    fs.copyFile(path.join(input, file), (path.join(output, file)),
+                        (err) => {
+                        if (err) throw err;
+                        }
+                    );
+                    console.log(file)
+                })
+            }
+        })
+    }  
+});
+
+
 
