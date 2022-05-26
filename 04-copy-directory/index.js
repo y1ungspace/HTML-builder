ad656d5ed1c4baa71dd1044e1031b03d213ff672
y1ungspace <y1ung.space@gmail.com>
2022-05-26 13:59:14 +0200

    fix: 04-copy-directory file was not deleted

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,7 +4,19 @@ const path = require('path');
 const input = path.join(__dirname, 'files');
 const output = path.join(__dirname, 'files-copy');
 
-fs.mkdir(output , {recursive: true}, err => {
+
+fs.readdir(output, (err, files) => {
+    if (err) return
+    else {
+        files.forEach(file => {
+            fs.unlink(path.join(output, file), err => {
+                if (err) throw err;
+            });
+        });
+    };
+});
+
+fs.mkdir(output , { recursive: true }, err => {
     if (err) throw err;
     else {
         fs.readdir(input, (err, files) => {
@@ -24,7 +36,6 @@ fs.mkdir(output , {recursive: true}, err => {
                         if (err) throw err;
                         }
                     );
-                    console.log(file)
                 })
             }
         })
